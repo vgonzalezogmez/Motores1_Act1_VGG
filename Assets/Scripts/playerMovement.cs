a35c0cc9b06a8a852bbc26249b8dca3f1dfd03c5
Víctor González Gómez <vgonzalezgomez@outlook.es>
2025-12-06 20:09:31 +0100

    update

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -9,7 +9,7 @@ public class playerMovement : MonoBehaviour
     public float velocidadGiro = 100.0f;
     public float velocidadAvance = 6.0f;
 
-    // Referencia al CharacterController [cite: 14, 32]
+    // Referencia al CharacterController 
     private CharacterController controller;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,21 +22,21 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // 1. Obtener Inputs del jugador [cite: 22, 23]
+        // 1. Obtener Inputs del jugador 
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
 
-        // 2. Calcular y aplicar el Giro (Rotate) [cite: 26]
-        // Usamos Time.deltaTime para suavizar el movimiento independiente de los FPS [cite: 28]
+        // 2. Calcular y aplicar el Giro (Rotate) 
+        // Usamos Time.deltaTime para suavizar el movimiento independiente de los FPS 
         float rotacion = movimientoHorizontal * velocidadGiro * Time.deltaTime;
         transform.Rotate(0, rotacion, 0);
 
         // 3. Calcular el Avance
-        // El personaje avanza hacia donde mira (Vector3.forward) [cite: 27]
-        // Multiplicamos por la velocidad, el input y el tiempo [cite: 25, 29]
+        // El personaje avanza hacia donde mira (Vector3.forward)
+        // Multiplicamos por la velocidad, el input y el tiempo 
         Vector3 avance = Vector3.forward * movimientoVertical * velocidadAvance * Time.deltaTime;
 
-        // 4. Transformar de Local Space a World Space [cite: 30, 31]
+        // 4. Transformar de Local Space a World Space [
         // Esto es crucial: convierte "adelante relativo a mí" en "coordenadas del mundo"
         Vector3 movimiento = transform.TransformDirection(avance);
 
