01f9fd5391a12a754810337496e02dc3f99c3b7d
Víctor González Gómez <vgonzalezgomez@outlook.es>
2025-12-21 19:14:02 +0100

    vgg

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -13,10 +13,15 @@ public class playerMovement : MonoBehaviour
     private CharacterController controller;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
         // Obtenemos el componente al iniciar
         controller = GetComponent<CharacterController>();
+    }
+    
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
